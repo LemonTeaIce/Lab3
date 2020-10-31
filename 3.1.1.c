df58bb8304929e34a644a0f32b457d39803874b7
mahfudz <2020971151@isiswa.uit,.edu.my>
2020-10-31 06:48:13 +0000

    commit for CTRL+Z

---
+++ 3.1.1.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+
+int main(void)
+{
+ void sigtstp_handler(int sig);
+ char s[200];
+
+ if(signal(SIGTSTP,sigtstp_handler)==SIG_ERR)
+ {
+  perror("signal");
+  exit(1);
+ }
+
+ printf("Enter a string:\n");
+
+ if(fgets(s,200,stdin)==NULL)
+ {
+  perror("gets");
+ }
+ else
+ {
+  printf("you entered:%s\n",s);
+ }
+return 0;
+}
+
+void sigtstp_handler(int sig)
+{
+ printf("This is a special signal handler for CTRL+Z!\n");
+}
